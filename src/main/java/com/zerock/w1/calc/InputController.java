cfeebf5c65b1b9b36442028786db83553832cad3
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-11-13 23:54:51 +0900

    End 1.3

---
+++ src/main/java/com/zerock/w1/calc/InputController.java
@@ -0,0 +1,24 @@
+package com.zerock.w1.calc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "inputController", urlPatterns = "/calc/input")
+public class InputController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws
+            ServletException, IOException{
+
+        System.out.println("InputController...doGet...");
+
+        // 서블릿에 전달된 요청을 다른 쪽으로 전달 혹은 배포
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/calc/input.jsp");
+        dispatcher.forward(req, res);
+    }
+}
